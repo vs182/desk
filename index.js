43cb033ef6423908be174365f67994d6fec2dbaf
hari.vs <hari.vs@zohocorp.com>
2023-04-03 23:28:07 +0530

    sec commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
 // Middleware to parse webhook body as JSON
 
@@ -24,6 +24,7 @@ app.post('/webhook', async (req, res) => {
 });
 
 // Start server
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+app.listen(process.env.PORT || PORT, function(err){
+  if (err) console.log("Error in server setup")
+  console.log("Server listening on Port", PORT);
+})
